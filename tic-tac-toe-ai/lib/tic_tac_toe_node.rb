97265f1eab0577eca2247e4f6848a7b3182aecfe
App Academy Student <imartinez921@gmail.com>
2022-06-13 17:38:53 -0400

    Partial completion tic-tac-toe

--- tic-tac-toe-ai/lib/tic_tac_toe_node.rb
+++ tic-tac-toe-ai/lib/tic_tac_toe_node.rb
@@ -1,27 +1,33 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @board = Board.new
-    @next_mover_mark = :x
-    if prev_move_pos == nil
-      @prev_move_pos = nil
-    else @prev_move_pos = prev_move_pos
-    end
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
+    return true if board.over? == true && board.winner != @next_mover_mark
+
   end
 
   def winning_node?(evaluator)
+  end
+
+  def [](pos)
+    row, col = pos[0], pos[1]
+    @rows[row][col]
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     empty_pos = []
-    (0...@board.length).each do |i|
-      (0...@board.length).each do |j|
+    (0..2).each do |i|
+      (0..2).each do |j|
         if @board.empty?([i,j])
           empty_pos << [i,j]
         end
@@ -29,12 +35,11 @@ class TicTacToeNode
     end
 
     children = []
+    @next_mover_mark == :x ? @next_mover_mark = :o : @next_mover_mark = :x
     empty_pos.each do |pos|
       temp_board = @board.dup
 
-      temp_board.place_mark(pos, @next_mover_mark)
-
-      @next_mover_mark == :x ? @next_mover_mark = :o : @next_mover_mark = :x
+      temp_board[pos] = @next_mover_mark
 
       children << TicTacToeNode.new(temp_board, @next_mover_mark, pos)
 
